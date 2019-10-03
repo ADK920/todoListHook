55b891b73707526d4e4dc4c8a177bd49491e094f
Andrew <andrew.karnis@gmail.com>
2019-10-02 20:26:43 -0400

    separate state

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,59 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ItemList from '../ItemList/ItemList';
 
 import './Todo.css';
 
 function TodoList() {
     const newItem = () => {
-        return { value: '', completed: false };
+        return { userId: 0, id: 2000, title: '', completed: false };
     }
-    const [state, setState] = useState({ item: newItem(), items: [] });
+
+    const [item, setItem] = useState(newItem());
+    const [items, setItems] = useState([]);
 
     const handleChange = evt => {
-        const item = { ...state.item };
-        item.value = evt.target.value;
-        setState({ ...state, item });
+        const updateItem = { ...item };
+        updateItem.title = evt.target.value;
+        setItem(updateItem);
     };
 
     const onAdd = () => {
-        const items = [...state.items];
-        items.push(state.item);
-        setState({ ...state, items, item: newItem() });
+        const addItems = [...items];
+        addItems.push(item);
+        setItems(items);
+        setItem(newItem());
     };
 
     const onComplete = (e, index) => {
-        const items = [...state.items];
-        const item = { ...items[index] };
-        item.completed = true;
-        items[index] = item;
-        setState({ ...state, items });
+        const completeItems = [...items];
+        const completedItem = { ...items[index] };
+        completedItem.completed = true;
+        completeItems[index] = item;
+        setItems(completeItems);
     };
 
     const onRemove = (e, index) => {
-        const items = [...state.items];
-        items.splice(index, 1);
-        setState({ ...state, items });
+        const removeItems = [...items];
+        removeItems.splice(index, 1);
+        setItems(removeItems);
     };
 
     const clearTodos = evt => {
-        setState({ ...state, items: [] });
+        setItems([]);
     };
-    const item = state.item;
-    const items = state.items;
-    return (
+
+    const getTodos = evt => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(res => res.json())
+            .then(res => setItems(res));
+    }
+
+    const content = (
         <div className='todo-list'>
             <h6>ToDo</h6>
-            <span>{item.value}</span>
+            <div>
+                <button onClick={getTodos}>Get more Todo!</button>
+            </div>
             <div>
                 <button onClick={clearTodos}>Clear All</button>
             </div>
             <div className='item-adder'>
-                <input value={item.value} onChange={handleChange}></input>
-                <button onClick={onAdd} disabled={item.value.length === 0}>Add Todo</button>
+                <input placeholder="Enter ToDo" value={item.title} onChange={handleChange}></input>
+                <button onClick={onAdd} disabled={item.title.length === 0}>Add Todo</button>
             </div>
             <ItemList items={items} onComplete={onComplete} onRemove={onRemove}></ItemList>
         </div>
     );
+    return content;
 }
 
 export default TodoList;
